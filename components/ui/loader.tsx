7108f99a99231ac87ffeebe874228b3e95ab1767
keycaped <99409468+ArhamF@users.noreply.github.com>
2025-01-17 23:25:33 -0500

    Update loader.tsx

--- components/ui/loader.tsx
+++ components/ui/loader.tsx
@@ -1,1 +1,8 @@
+import React from 'react'
+
+export function Loader({ className }: { className?: string }) {
+  return (
+    <div className={`animate-spin rounded-full h-4 w-4 border-t-2 border-b-2 border-gray-900 ${className}`}></div>
+  )
+}
 
